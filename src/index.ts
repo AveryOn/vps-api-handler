384647ecbe1ae74afd292b8724ca4cd6b319c01c
AveryOn <lnkonion2@gmail.com>
2025-05-03 11:50:28 +0500

    fix errors

--- src/index.ts
+++ src/index.ts
@@ -52,12 +52,15 @@ app.post(
         try {
           await gitHubWebhookHandler(body.payload, body.event)
           res.status(200).send('Deploy triggered')
+          return
         } catch (err) {
           console.error(err)
           res.status(500).send('Deploy failed')
+          return
         }
       }
       res.status(200).send('OK')
+      return
     }
   )
 
