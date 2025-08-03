f1d41ad675174e2a6b372dbf6e5186e05549868c
AveryOn <lnkonion2@gmail.com>
2025-08-03 22:44:38 +0400

    fix

--- src/services/webhooks.service.ts
+++ src/services/webhooks.service.ts
@@ -164,7 +164,7 @@ export async function gitHubWebhookHandler(
                             end_at: formatDate(),
                             execution_time: String(Date.now() - nowMs),
                         })
-                        resolve(void 0);
+                        resolve(void 0)
                     })
                 })
                 // exec(`bash -lc 'source ~/.nvm/nvm.sh && pm2 reload ${ServiceNamesByProject[payload?.repository?.name]}`)
