e2c2560a4045909f63e49302bb5fe94441e04aeb
AveryOn <lnkonion2@gmail.com>
2025-08-03 21:58:46 +0400

    fix

--- src/const/global.ts
+++ src/const/global.ts
@@ -2,5 +2,5 @@ import { fileURLToPath } from "url"
 import path from 'path'
 
 // эмулируем __dirname в ESM
-export const __filename = fileURLToPath(import.meta.url)
-export const __dirname = path.dirname(__filename)
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
